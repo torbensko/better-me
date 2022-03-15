26c95e8abc73ebe5c6e7e40092753295b597a412
Torben Sko <torben.sko@nextpracticehealth.com>
2022-03-15 17:59:38 +1100

    Blank ToggleGroup

---
+++ client/src/components/ToggleGroup/index.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+export interface IToggleGroupProps {}
+
+export const ToggleGroup: React.FC<IToggleGroupProps> = ({}) => {
+  return <div>My ToggleGroup</div>;
+};
---
+++ client/src/components/ToggleGroup/readme.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+import { ToggleGroup } from ".";
+
+export const DemoStandard = () => {
+  return <ToggleGroup />;
+};
