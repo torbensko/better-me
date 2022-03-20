ebd7ece0c8912aa4990ce69b4628e2c1d7539872
Torben Sko <torben.sko@nextpracticehealth.com>
2022-03-20 19:06:19 +1100

    Fixed an issue with the Knexfile

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,11 +1,18 @@
 require('dotenv').config();
 
+const parts = process.env.DATABASE_URL.match(/^postgres:\/\/([^:]+):([^@]*)@([^:]+):(\d+)\/([\w\d]+)/);
+
 module.exports = {
   client: "pg",
-  connection: process.env.DATABASE_URL,
-  pool: {
-    min: 2,
-    max: 10
+  // connection: process.env.DATABASE_URL,
+  connection: {
+    user: parts[1],
+    password: parts[2],
+    host: parts[3],
+    database: parts[5],
+    charset: "utf8",
+    pool: { min: 0, max: 50 },
+    ssl: { rejectUnauthorized: false }
   },
   migrations: {
     extension: "js",
