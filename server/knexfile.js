47a486a83807d2719b10c4c8419c9ba3b8141be4
Torben Sko <torben.sko@nextpracticehealth.com>
2022-03-27 18:22:47 +1100

    Fixed an issue with the remote DB

--- server/knexfile.js
+++ server/knexfile.js
@@ -12,7 +12,7 @@ module.exports = {
     database: parts[5],
     charset: "utf8",
     pool: { min: 0, max: 50 },
-    // ssl: { rejectUnauthorized: false }
+    ssl: { rejectUnauthorized: false }
   },
   migrations: {
     extension: "js",
